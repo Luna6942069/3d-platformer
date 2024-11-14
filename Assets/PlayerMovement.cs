b8e6768fc370ce301602bcf6d0cd364ed35f8291
Luna6942069 <lneacsu2024@elam.co.uk>
2024-11-14 15:41:16 +0000

    ajjajjaja

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -13,15 +13,30 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] LayerMask ground;
 
     [SerializeField] AudioSource jumpSound;
+
+    [SerializeField] float mouseSensitivity = 100f;
+    float xrotation = 0;
+    [SerializeField] Transform playercamera;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody> ();
+        Cursor.lockState = CursorLockMode.Locked;
+
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+       //
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
