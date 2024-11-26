f222385d76c89f5400c5a8cf8bc0ee0908fce63d
Luna6942069 <lneacsu2024@elam.co.uk>
2024-11-26 11:44:44 +0000

    balls

--- Assets/Start.cs
+++ Assets/Start.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class Start : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
+        SceneManager.LoadScene("Level01");
     }
 }
