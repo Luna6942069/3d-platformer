6b63d963fe3a2a217f32ad91ed7762c2a3bd20d9
Luna6942069 <lneacsu2024@elam.co.uk>
2024-11-26 10:44:37 +0000

    button kinda

--- Assets/Start.cs
+++ Assets/Start.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Start : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
